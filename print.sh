858db602d01247b36a2662d44f27be00d02a529a
jbcdnr <jb.cdnr@gmail.com>
2014-05-20 17:44:25 +0200

    modified printing process

--- print.sh
+++ print.sh
@@ -1,30 +1,19 @@
 #!/bin/sh
-# /!\ files don't start with a '-'
-# No space in names and passwords
-# input : login, password, files, -o options
 
 if test $# -lt 3; then
   echo "Missing arguments" 2>&1;
   exit -1;
 fi
 
-login=$1; shift
-password=$1; shift
+login="$1"; shift
+password="$1"; shift
 file="$1"; shift
-myOptions="$1";
-options="$@";
+options="$1";
 
-echo "user : " $login 
-echo "file : " $file
-echo "options : " $options;
 printer="Pool1-$login-$RANDOM";
 
-
 lpadmin -p $printer -E -v smb://INTRANET/$login:$password@print1.epfl.ch/pool1 -P /usr/share/cups/model/xr_WorkCentre7655R.ppd.gz 2>&1
-#echo "lpadmin -p $printer -E -v smb://INTRANET/$login:$password@print1.epfl.ch/pool1 -P /usr/share/cups/model/xr_WorkCentre7655R.ppd.gz"
-
-  echo "lp -d $printer $file $options"
-  lp -d $printer $file $options
+lp -d $printer $file $options
 
 sleep 2
 
