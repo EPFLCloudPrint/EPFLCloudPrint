7cb960b2c544029e45e94287a6d386a5980650b5
Charles Gallay <charles.gallay@epfl.ch>
2014-05-21 21:43:52 +0200

    Try to verify Username and passwd

--- print.php
+++ print.php
@@ -1,25 +1,50 @@
 <?php
+function isCredentialCorrect($user,$passwd){
+	$url = 'https://tequila.epfl.ch/cgi-bin/tequila/login';
+	$data = array('username' => $user, 'password' => $passwd);
+	
+	// use key 'http' even if you send the request to https://...
+	$options = array(
+	    'http' => array(
+	        //'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+	        'method'  => 'POST',
+	        'content' => http_build_query($data),
+	    ),
+	);
+	$context  = stream_context_create($options);
+	$result = file_get_contents($url, false, $context);
+	
+	$DOM = new DOMDocument;
+	$DOM->loadHTML($result);
+	//$titre = $DOM->getElementsByTagName('title');
+	//echo $titre->item(0)->nodeValue;
+	return getElementsByTagName('title')->item(0)->nodeValue==="Tequila";
+}
 
 $SCRIPT="print.sh";
 $answer = array("error_code" => 0);
+if(!isCredentialCorrect($_POST["user"],$_POST["password"])){
+	$answer = array("error_code" => 1000);
+} else {
 
-$options = array();
+	$options = array();
 
-if($_POST["selection_selected"] === "on"){
-  array_push($options, "-P " . $_POST["from"] . "-" . $_POST["to"]);
+	if($_POST["selection_selected"] === "on"){
+	  array_push($options, "-P " . $_POST["from"] . "-" . $_POST["to"]);
+	}
+
+	array_push($options, "-n " . $_POST["number_copies"]);
+
+	if($_POST["double_sided"] === "on"){
+	  array_push($options, "-o sides=two-sided-long-edge");  
+	}
+
+	array_push($options, "-t " . $_POST["server_file_name"]);
+
+	$cmd = "./" . $SCRIPT . " '" . $_POST["user"] . "' '" . $_POST["password"] . "' 'uploads/" . $_POST["server_file_name"] . "' '" . join(" ", $options) . "'";
+	$answer["command"] = $cmd;
+	shell_exec($cmd." >stdout.log 2>stderr.log");
 }
-
-array_push($options, "-n " . $_POST["number_copies"]);
-
-if($_POST["double_sided"] === "on"){
-  array_push($options, "-o sides=two-sided-long-edge");  
-}
-
-array_push($options, "-t " . $_POST["file_name"]);
-
-$cmd = "./" . $SCRIPT . " '" . $_POST["user"] . "' '" . $_POST["password"] . "' 'uploads/" . $_POST["server_file_name"] . "' '" . join(" ", $options) . "'";
-$answer["command"] = $cmd;
-shell_exec($cmd." >stdout.log 2>stderr.log");
-
 echo json_encode($answer);
+
 ?>
