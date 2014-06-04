a33dcd37d8b989e8af13f2e695dc8bd3cb0b3668
jbcdnr <jb.cdnr@gmail.com>
2014-06-04 22:46:26 +0200

    Brand new design ! Only in demo for the moment, need form verification, link with the back-end, cleaning and debugging

--- script.js
+++ script.js
@@ -1,169 +1,129 @@
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-  /* 
-    UPLOAD FILE FUNCTIONS
-  */
+  /* MY FRAMEWORK */
 
-  var preventDefault = function(e) {
-    e.stopPropagation();
-    e.preventDefault();
-  }
-
-  var beforeSubmit = function() {
-    $('#cloud-text-container').hide();
-    $('#dialog').slideDown(1500);
-    $('#submit').prop("disabled",true);
-  }
-
-  var uploadProgress = function(e) {
-    $("#cloud-bar").width(parseInt(100*(e.loaded/ e.total)) + "%");
-  }
-
-  var complete = function(e) {
-    var rep = JSON.parse(e.currentTarget.responseText);
-    $('#cloud-text').css('color', 'white');
-    $('#cloud-text').css("font-size", "18px");
-    if(rep.error_code != 0) {
-      $('#server_file_name').val("");
-      $('#file_name').val("");
-      $('#cloud-text').text("An error occured while uploading the file...");
-      $('#submit').prop("disabled",true);
+  $('.checkbox').on("click", function() {
+    event.preventDefault();
+    if($(this).hasClass('checked')) {
+      $(this).removeClass('checked').addClass('unchecked');
     } else {
-      $('#server_file_name').val(rep.server_file_name);
-      $('#file_name').val(rep.file_name);
-      $('#cloud-text').text("You uploaded " + rep.file_name);
-      $('#submit').prop("disabled",false);
+      $(this).removeClass('unchecked').addClass('checked');
     }
+    $(this).trigger("classChanged");
+  });
 
-    str = $('#cloud-text').text();
-    if(str.length > 70) {
-      $('#cloud-text').text(str.substr(0,30) + "..." + str.substr(str.length - 30, str.length - 1));
+  $('.radiobox').on("click", function() {
+    event.preventDefault();
+    if($(this).hasClass('unchecked')) {
+      $(this).parents('.radioGroup').children('.radiobox').removeClass('checked').addClass('unchecked');
+      $(this).removeClass('unchecked').addClass('checked');
+      $(this).parents('.radioGroup').children('.radiobox').trigger("classChanged");
     }
-    $('#cloud-text-container').fadeIn(1000);
+  });
+
+  $('.label').on("click", function() {
+    event.preventDefault();
+    $('.' + $(this).attr('for')).click();
+  });
+
+  /* PAGE ORGANISATION */
+
+  var centering = function() {
+    if($('.container').width() >=  768) {
+      /* cloud position */
+      var h = Math.max(0, ($('.container').height() - $('.footer').height() - $('.foreground').height()) / 2);
+      $('.cloud').css('margin-top', h);
+
+      /* dialog position */
+      var h = Math.max(0, ($('.container').height() - $('.footer').height() - $('.dialog').height()) / 2);
+      $('.dialog').css('margin-top', h);
+    } else {
+      $('.cloud').css('margin-top', 0);
+      $('.dialog').css('margin-top', 0);
+    }
   }
 
-  var uploadFile = function(file) {
-    beforeSubmit();
-    var fd = new FormData();
-    fd.append('file', file );
-    var xhr = new XMLHttpRequest();
-    xhr.upload.addEventListener("progress", uploadProgress);
-    xhr.addEventListener("loadend", complete);
-    xhr.open("POST", "upload_file.php");
-    xhr.send(fd);
+  var continuousCentering = function() {
+    var proc = setInterval(centering, 10);
+    setTimeout(function() {
+      clearInterval(proc);
+    }, 3000);
   }
 
-  /*
-    DRAG AND DROP
-  */
+  $(window).resize(centering);
+  $('.dialog').on('heightChange', continuousCentering);
 
-	//block drag and drop on others part
-	$(document).on('drop',preventDefault);
-	$(document).on('dragenter',preventDefault);
-	$(document).on('dragover', preventDefault);
+  centering();
 
-	$("#fileSelect").on('dragenter', preventDefault);
-	$("#fileSelect").on('dragover', preventDefault);
-	$("#fileSelect").on('drop', function (e){
-		var files = e.originalEvent.dataTransfer.files;
-		uploadFile(files[0]);
-	});
+  /* footer position */
+  var h = $('.footer').height();
+  var w = $('.footer').width();
+  $('.cloud').css('padding-bottom', h);
+  $('.dialog').css('padding-bottom', h);
+  $('.footer').css('height', h);
 
-	/*
-		UPLOAD FILE FORM
-	*/
+  /* CLOUD PROGRESSION */
+  var setProgression = function(percentage) {
+    $("#tick_path").hide();
+    $('stop').attr('offset', percentage+'%');
+  }
 
-	// cloud reactions
-	$('#fileSelect').click(function(e) {
-	  $('#fileElem').click();
+  var endUpload = function() {
+    setProgression(0);
+    $("#tick_path").show();
+    $(".cloud p").hide();
+  }
+
+  var printed = function() {
+    $('.options').hide();
+    $('#upload_button').html("UPLOAD NEW FILE");
+    $('#upload_button').show();
+    $('.dialog').trigger("heightChange");
+  }
+
+  /* PAGE REACTIONS */
+
+  /* upload button reaction */
+  $('#upload_button').on("click", function() {
+    $('h1').attr('style', 'margin-bottom: 40px;');
+    $('#upload_button').hide();
+    $('.options').slideDown(1000);
+    $('.cloud p').fadeIn(1000);
+    $('.dialog').trigger('heightChange');
+    uploadFile();
   });
 
-  // form submission
-	$("#formUpload").change(function(e) {
-		uploadFile($("#fileElem")[0].files[0]);
-	});
-
-  /*
-  	DIALOG FORM
-  */
-
-  // Form reactions
-
-  $('#selection_all').on('click', function() {
-    $('.from_to').slideUp('slow');
-    $('#selection_selected').prop("checked", false);
-  });
-
-  $('#selection_selected').on('click', function() {
-    $('.from_to').slideDown('slow');
-    $('#selection_all').prop("checked", false);
-  });
-
-  $("#from").on("change", function() {
-    var v = $("#from").val();
-    if(v !== parseInt(v)) {
-      $("#to").prop("min", v);
-      if($("#to").val() < v) {
-        $("#to").val(v);
-      }
+  /* page selection hide/show */
+  $('.selectedonly').on("classChanged", function() {
+    if($(this).hasClass('checked')) {
+      $('#fromto').slideDown('normal');
+      $('#from').val("");
+      $('#to').val("");
+    } else {
+      $('#fromto').slideUp('normal');
     }
+    $('.dialog').trigger('heightChange');
   });
 
-  // Form submission
+  /* FUNCTIONNALITIES */
 
-	var options_dialog = {
-		beforeSubmit: function(arr, $form, options) {
-			$("#error").hide(0);
-		  $("#error").html("Form could not be validated :<br/><ul></ul>");
-      $("#print-result").hide(0);
+  var uploadFile = function() {
+    progressing();
+  }
 
-		  var error = false;
+  /* DEMO */
 
-		  if($("#user").val() === "") {
-			  $("#error ul").append('<li>User field is empty</li>');
-		  	error = true;
-		  }
+  $(".print").on("click", printed);
 
-	  	if($("#password").val() === "") {
-	  		$("#error ul").append('<li>Password field is empty</li>');
-	  		error = true;
-	  	}
-
-		  if(parseInt($("#from").val()) > parseInt($("#to").val())) {
-		  	$("#error ul").append('<li>Wrong page numbers</li>');
-			  error = true;
-		  }
-
-		  if(error) {
-		  	$("#error").show(0);
-		  }
-
-		  return ! error;
-  	},
-
-  	complete: function(response) {
-	  	var rep = JSON.parse(response.responseText);
-	  	if(rep.error_code == 0) {
-	  		$("#print-result").text("The document was successfully printed.");
-	  		$("#print-result").removeClass("alert-danger").addClass("alert-success");
-	  		$("#print-result").show(0);
-	  		setTimeout("location.reload()", 2000);
-	  	} else {
-        if(rep.error_code == 1000) {
-          $("#print-result").text("Please verify your credentials...");
-        } else {
-          $("#print-result").text("An error occured while printing the document...");
-        }
-	  		$("#print-result").removeClass("alert-success").addClass("alert-danger");
-	  		$("#print-result").show(0);
-	  	}
-	  }
-	}
-
-	$("#dialog-form").submit(function(event) {
-		event.preventDefault();
-		$(this).ajaxSubmit(options_dialog);
-	});
+  var progressing = function() {
+    var p = parseInt($('stop').attr('offset').replace('%', ''));
+    if(p < 100) {
+      setProgression(p + 1);
+      setTimeout(progressing, 50);
+    } else {
+      setTimeout(endUpload, 500);
+    }
+  }
 
 });
