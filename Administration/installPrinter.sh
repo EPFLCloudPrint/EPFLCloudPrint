12a4536bb16542f0429337392a33c4e99623229d
Charles Gallay <charles.gallay@epfl.ch>
2014-06-23 16:50:08 +0200

    rm

--- Administration/installPrinter.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-echo "Adding a printer named mainPrinter\n";
-
-lpadmin -p mainPrinter -E -v lpd://print1.epfl.ch/pool1 -P /usr/share/cups/model/xr_WorkCentre7655R.ppd
-
-echo "printer created\n";
