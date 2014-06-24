07f8963b682541abba93a290e80ee1a908cb38cc
Giacomo Giudice <giacomo.giudice@mail.com>
2014-06-24 16:01:16 +0200

    update bash

--- Administration/install.sh
+++ Administration/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 if [ $(uname) = "Linux" ]; then
-	echo "Linux configuaration detected\n";
+	echo "Linux configuaration detected";
 	PPD_EXTENTION=".gz";
 else
 	if [ $(uname) = "Darwin" ]; then
-		echo "Mac OS configuaration detected\n";
+		echo "Mac OS configuaration detected";
 		PPD_EXTENTION="";
 	else
 		echo "Unknow Operation System";
@@ -12,14 +12,17 @@ else
 	fi
 fi
 
-echo "create directory CloudPrintUpload for uploading files in /tmp:\n";
-rm -r /tmp/CloudPrintUpload;
-#Create a folder that everybody can write in 
-mkdir -m o+wr /tmp/CloudPrintUpload;
+echo "create directory CloudPrintUpload for uploading files in /tmp";
+dir="/tmp/CloudPrintUpload";
+if [ -e $dir ]
+then
+	rm -r $dir;
+fi
+#Create a folder that everybody can write in
+mkdir -m o+wr $dir;
 
 
 echo "Adding a printer named mainPrinter\n";
 lpadmin -x mainPrinter;
 
 lpadmin -p mainPrinter -E -v lpd://print1.epfl.ch/pool1 -P /usr/share/cups/model/xr_WorkCentre7655R.ppd$PPD_EXTENTION;
- 	
